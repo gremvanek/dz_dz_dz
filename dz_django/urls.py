94268c991b112ff075304db5683406c4f65b1935
gremv <gremvanek@gmail.com>
2024-03-07 12:14:44 +0500

    Забыл добавить урлы для admin.

--- dz_django/urls.py
+++ dz_django/urls.py
@@ -1,7 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
 from django.urls import path
+
+import config
 from config import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('contacts/', views.contacts, name='contacts'),
-]
+                  path('', views.home, name='home'),
+                  path('contacts/', views.contacts, name='contacts'),
+                  path('admin/', admin.site.urls),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

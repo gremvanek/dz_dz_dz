bb85e3840a364081c534c6f71b5206a5c3d13d18
gremv <gremvanek@gmail.com>
2024-03-11 22:40:53 +0500

    @register.filter добавил name='image_tags'. product_details.html переписал. Оказалось, что у меня шаблон вне блока был )). В views.py небольшие изменения.

--- config/views.py
+++ config/views.py
@@ -22,7 +22,6 @@ def contacts(request):
 
 
 def product_detail(request, pk):
-    # Используем get_object_or_404 для загрузки продукта по PK или возвращения 404 ошибки, если продукт не найден
-    product = get_object_or_404(Product, pk=pk)
+    product = get_object_or_404(Product, **{'pk': pk})
     context = {'product': product}
     return render(request, 'product_details.html', context)

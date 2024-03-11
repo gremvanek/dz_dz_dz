896c4aa80ce6ee3ceaaf794b22a1a91c0b2b9328
gremv <gremvanek@gmail.com>
2024-03-11 22:40:49 +0500

    @register.filter добавил name='image_tags'. product_details.html переписал. Оказалось, что у меня шаблон вне блока был )). В views.py небольшие изменения.

--- config/templatetags/image_tags.py
+++ config/templatetags/image_tags.py
@@ -3,7 +3,7 @@ from django import template
 register = template.Library()
 
 
-@register.filter
+@register.filter(name='image_tags')
 def media_url(image_path):
     if image_path:
         return f'/media/{image_path}'

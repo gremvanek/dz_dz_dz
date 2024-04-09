7fb36530b1cbef4636c64e7d8058f563dae3286f
gremv <gremvanek@gmail.com>
2024-04-10 00:46:40 +0500

    Реализация сброса пароля.

--- users/views.py
+++ users/views.py
@@ -1,14 +1,20 @@
+import random
+
 from django.contrib import messages
 from django.contrib.auth import get_user_model, authenticate
-from django.contrib.auth.tokens import default_token_generator as token_generator
+from django.contrib.auth.forms import PasswordResetForm
+from django.contrib.auth.forms import default_token_generator as token_generator
+from django.contrib.auth.hashers import make_password
+from django.core.mail import send_mail
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.utils.encoding import force_text
 from django.utils.http import urlsafe_base64_decode
+from django.utils.translation import gettext_lazy as _
 from django.views import View
-from django.views.generic import CreateView, UpdateView
+from django.views.generic import FormView, CreateView, UpdateView
 
-from users.forms import UserProfileForm, UserRegisterForm
+from users.forms import UserRegisterForm, UserProfileForm
 from users.utils import send_email_for_verify
 
 User = get_user_model()
@@ -75,3 +81,36 @@ class EmailVerifyView(View):
             # Если токен недействителен, выводим сообщение об ошибке
         messages.error(request, "Ссылка для подтверждения недействительна или истекла")
         return redirect('users:email_fail')
+
+
+class CustomPasswordResetView(FormView):
+    template_name = 'users/password_reset.html'
+    form_class = PasswordResetForm
+    success_url = reverse_lazy('users:login')
+
+    def form_valid(self, form):
+        # Генерация случайного пароля
+        random_password = ''.join(
+            random.choices('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', k=12))
+        # Хеширование пароля
+        hashed_password = make_password(random_password)
+
+        # Получение адреса электронной почты из формы
+        email = form.cleaned_data['email']
+
+        # Обновление пароля пользователя с указанным адресом электронной почты
+        for user in form.get_users(email):
+            user.set_password(hashed_password)
+            user.save()
+
+            # Отправка нового пароля на адрес электронной почты пользователя
+            send_mail(
+                _('Password reset'),
+                _('Your new password is: {}').format(random_password),
+                'from@example.com',
+                [email],
+                fail_silently=False,
+            )
+
+        # После успешного сброса пароля происходит редирект на страницу входа
+        return super().form_valid(form)
